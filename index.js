edb06c9c2e4ebc6d0fc430a318d7948b8d7a9a6e
Idris110 <ratlamwalaidris110@gmail.com>
2023-09-02 12:28:20 +0530

    home page

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-
+const express = require('express');
 const WebSocket = require('ws');
+const cors = require("cors");
 
+const app = express();
 const server = new WebSocket.Server({ port: 59898 });
 let socList;
 let init = false;
+
+app.use(
+  cors({
+      origin:"http://localhost:3000",
+  })
+)
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Home page")
+});
 
 server.on('connection', (socket, req) => {
   console.log('Connection from', req.socket.remoteAddress);
