2b61a7b9a9982175372338f83b82183c615dc5cb
Idris110 <ratlamwalaidris110@gmail.com>
2023-09-12 20:12:59 +0530

    index.html

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
-// const INDEX = '/index.html';
+const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
